1e9b8fa062f952d3d85e55e245c8cc39a3aadd48
saifchaudhry <schaudhry722@gmail.com>
2024-06-11 16:37:02 +0500

    added jenkins files in test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # Association test
+  
   it { should have_many(:favorites).dependent(:destroy) }
   it { should have_many(:favorite_movies).through(:favorites) }
-
   # Validation tests 
   # ensure columns username is present and unique
   it { should validate_presence_of(:username) }
