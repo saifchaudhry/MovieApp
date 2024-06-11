07e93c3293981cacb1677c6dddc505e951b78a58
Saifchaudhry <37732679+saifchaudhry@users.noreply.github.com>
2024-06-11 18:34:49 +0500

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -4,28 +4,38 @@ export DEBIAN_FRONTEND=noninteractive
 current_user=$(whoami)
 echo "Current user is: $current_user"
 
-# Check if rbenv is installed
-if ! command -v rbenv &> /dev/null
-then
-    echo "rbenv could not be found, installing..."
-    sudo apt-get update && sudo apt-get install -y \
-    autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev \
-    postgresql postgresql-client postgresql-contrib libpq-dev
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" &> /dev/null
+}
 
-    # Install rbenv and ruby-build
-    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-    cd ~/.rbenv && src/configure && make -C src
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-    source ~/.bashrc
-    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-    echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-    source ~/.bashrc
+# Check if running as root
+if [ "$current_user" != "root" ]; then
+    echo "Please run as root or use sudo"
+    exit 1
+fi
+
+# Install system dependencies
+apt-get update && apt-get install -y \
+autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev \
+postgresql postgresql-client postgresql-contrib libpq-dev git curl
+
+# Check if rbenv is installed
+if ! command_exists rbenv; then
+    echo "rbenv could not be found, installing..."
+    git clone https://github.com/rbenv/rbenv.git /usr/local/rbenv
+    cd /usr/local/rbenv && src/configure && make -C src
+    echo 'export PATH="/usr/local/rbenv/bin:$PATH"' >> /etc/profile.d/rbenv.sh
+    echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
+    source /etc/profile.d/rbenv.sh
+    git clone https://github.com/rbenv/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+    echo 'export PATH="/usr/local/rbenv/plugins/ruby-build/bin:$PATH"' >> /etc/profile.d/rbenv.sh
+    source /etc/profile.d/rbenv.sh
 else
     echo "rbenv is already installed"
 fi
 
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="/usr/local/rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 # Install Ruby 2.6.6
@@ -44,4 +54,3 @@ if ! gem list bundler -i -v 2.0.1 > /dev/null 2>&1; then
 else
     echo "Bundler 2.0.1 is already installed"
 fi
-
