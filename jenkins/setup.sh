b825e7d14afcc878ce77a4d416f85eba44bac6d8
Saifchaudhry <37732679+saifchaudhry@users.noreply.github.com>
2024-06-11 16:18:29 +0500

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -6,20 +6,21 @@ echo "Current user is: $current_user"
 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-export PATH=$(echo $PATH | awk -v RS=: -v ORS=: '!($0 in seen) {seen[$0]; print}')
-source ~/.bashrc
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 
 apt-get update && apt-get install -y \
 autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev \
 postgresql postgresql-client postgresql-contrib libpq-dev
 
-if  [ "$(ruby -v | awk '{print $2}' | cut -d 'p' -f 1)" == "2.6.6" ]; then
+if rbenv versions | grep -q "2.6.6"; then
     echo "Ruby 2.6.6 is installed"
 else
     echo "Ruby 2.6.6 not found!"
     rbenv install 2.6.6
-    rbenv global 2.6.6
 fi
+
+rbenv global 2.6.6
 
 if gem list bundler -i -v 2.0.1 > /dev/null 2>&1; then
     echo "Bundler 2.0.1 is installed"
