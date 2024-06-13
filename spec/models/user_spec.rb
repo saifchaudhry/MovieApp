b343f364b2b7aeae77da079a6a6087fbf8a624f8
saifchaudhry <schaudhry722@gmail.com>
2024-06-13 18:20:34 +0500

    final testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe User, type: :model do
   # Validation tests 
   # ensure columns username is present and unique
 
-  
+
   it { should validate_presence_of(:username) }
+  
   subject { FactoryBot.build(:user) }
   it { should validate_uniqueness_of(:username) }
 end
